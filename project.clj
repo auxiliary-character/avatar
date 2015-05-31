b9dfaa949ccf2f4c8700bc81607c6fdb3bd16d55
Austin Fuller <auxchar@gmail.com>
2015-05-31 16:59:52 -0500

    Fixed some readme/project.clj stuff.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject avatar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "This is my avatar, and how I made it."
+  :url "https://github.com/auxiliary-character/avatar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
